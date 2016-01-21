0fc8761ad7b03ebcd719dfc76606d0f07d083d0b
Justin Macedo <justinmacedo@ufl.edu>
2016-01-20 20:10:29 -0500

    finished assignment 3

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -3,6 +3,8 @@
   Import modules/files you may need to correctly run the script. 
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
+
+var data = require('./listings.json'); 
 var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
@@ -10,11 +12,37 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri, function(err){
+  if(err){
+    throw err;
+  }
+  console.log('connected!');
+  for(var i=0; i<data.entries.length; i++){
+    var list = Listing({
+      code: data.entries[i].code,
+      name: data.entries[i].name,
+      coordinates: data.entries[i].coordinates,
+      address: data.entries[i].address, 
+    });
+
+    list.save(function(err, list){
+      if (err) throw err;
+
+      console.log(list);
+    // saved!
+    })
+  }
+
+});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+
+
+
+
 
 
 /* 
--- queries.js
+++ queries.js
@@ -1,10 +1,25 @@
 /* Fill out these functions using Mongoose queries*/
+var config = require('./config');
+var mongoose = require('mongoose');
+var Listing = require('./ListingSchema.js')
+/* Connect to your database */
+mongoose.connect(config.db.uri, function(err){
+  if(err){
+    throw err;
+  }
+});
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({'code' : 'LBW'}, function(err,list){
+    if (err){
+      throw err;
+    }
+    console.log(list);
+  });
 };
 var removeCable = function() {
   /*
@@ -12,17 +27,45 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.remove({'code' : 'CABL'}, function(err,list){
+    if (err){
+      throw err;
+    }
+    console.log(list);
+  });
+
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+   Listing.update({'address':'701 N Broadway, Sleepy Hollow, NY 10591, United States'},{$set:{'address':'100 Phelps Lab P.O. Box 116350, Gainesville, FL  32611'}}, function(err, list){
+    if (err){
+      throw err;
+    }
+    console.log(list);
+  });
+
+  Listing.findOne({'code' : 'PHL'}, function(err,list){
+    if (err){
+      throw err;
+    }
+    console.log(list);
+  });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
+  Listing.find({}, function(err,list){
+    if (err){
+      throw err;
+    }
+    console.log(list);
+  });
+
 };
 
 findLibraryWest();
